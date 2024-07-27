a38eea9cc841ed8012b2f9334ca56c6c9803d3f8
fola oyewole <fola057@gmail.com>
2024-07-27 09:08:35 +0100

    feature-htmx: using htmx post to reload todo list

--- src/TodoApp/HomeHTML.hs
+++ src/TodoApp/HomeHTML.hs
@@ -6,7 +6,19 @@ import Lucid
 import TodoApp.Models (Todo (description, todoId))
 
 import qualified Data.Text as T
+import Lucid.Base (TermRaw(termRaw))
 
+todoRow :: Todo -> Html ()
+todoRow todo =  do
+  li_ [class_ "bg-white p-2 shadow-md rounded"] $ do
+    div_ [class_ "space-x-3 flex justify-between" ] $ do
+      span_ [class_ "text-black text-[13px] font-medium"] (toHtml (description todo) :: Html ())
+      deleteBtn $ todoId todo
+
+todoResults :: [Todo] -> Html ()
+todoResults todosList = do
+   div_ [class_ "", termRaw "id" "new-result"] $ do
+          ul_ [class_ "space-y-2"] $ mapM_ todoRow todosList
 
 deleteBtn :: Int -> Html()
 deleteBtn todoId = do
@@ -20,17 +32,16 @@ page todoList = do
   html_ $ do
     head_ $ do
       link_ [href_ "output.css" , rel_ "stylesheet"]
+      script_ [src_ "https://unpkg.com/htmx.org@2.0.1", integrity_ "sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/", crossorigin_ "anonymous"] (""::Text)
     body_  $ do
       div_ [class_ "m-auto w-1/2 mt-10 bg-gray-300 p-5 rounded md:w-[350px] lg:w-[500px]"] $ do
         h2_ [class_ "text-center text-xl font-bold"] "TodoApp"
         div_ [class_ "p-3"] $ do
-          form_ [class_ "h-20", action_ "/addTodo", method_ "POST"] $ do
+          form_ [class_ "h-20", termRaw "hx-post" "/addTodo" , termRaw "hx-target" "#new-result", termRaw "hx-swap" "outerHTML"] $ do
             input_ [ class_ "rounded h-8 px-2 w-full", type_ "text", name_ "todo_item", placeholder_ "Enter a todo item"]
             input_ [ class_ "bg-green-700 text-white h-8 w-full mt-6", type_ "submit", value_ "Add todo"]
-        div_ [class_ ""] $ do
-          ul_ [class_ "space-y-2"] $ mapM_ (\todo -> li_[class_ "bg-white p-2 shadow-md rounded"] $ div_ [class_ "space-x-3 flex justify-between"] $ do
-            toHtml(description todo) 
-            deleteBtn $ todoId todo
-           )todoList 
 
-homePageHtml todoList = do renderText(page todoList)
+        todoResults todoList
+
+
+homePageHtml todoList = do renderText (page todoList)
